1c65ab9edd0c7c81fe364c075acad2c6dc80636f
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-11 16:42:45 +0200

    Implement Auth0 user system

---
+++ src/logout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from '@mui/material';
+
+function LogoutButton() {
+    const { logout } = useAuth0();
+
+    return <Button onClick={() => logout({ returnTo: window.location.origin })} sx={{ fontWeight: 'bolder', fontSize: 17, '&:hover': { color: 'black' } }} disableRipple style={{ backgroundColor: 'transparent' }} color='error'>Log Out</Button>
+}
+
+export default LogoutButton;
