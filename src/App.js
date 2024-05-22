68aaeec8275f53a6f58b02a7a1bc4ec003d9dd6f
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-22 16:01:07 +0200

    Make changes to language fixer, store choice of lang in localstorage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import WeatherApp from './Components/WeatherApp';
-import LanguageSwitcher from './Components/LanguageSwitcher';
 
 const App = () => {
   return (
     <div>
-      <LanguageSwitcher />
       <WeatherApp />
     </div>
   );
--- src/Components/LanguageSwitcher.js
+++ src/Components/LanguageSwitcher.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const LanguageSwitcher = () => {
     const { i18n } = useTranslation();
+    const [language, setLanguage] = useState(localStorage.getItem('language') || 'en');
+
+    useEffect(() => {
+        localStorage.setItem('language', language);
+        i18n.changeLanguage(language);
+    }, [language, i18n]);
 
     const changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
+        setLanguage(lng);
     };
 
     return (
         <div>
-            <button onClick={() => changeLanguage('en')}>EN</button>
-            <button onClick={() => changeLanguage('no')}>NO</button>
+            {language === 'en'
+                ? <img onClick={() => changeLanguage('no')} alt='United States Flag' src='/images/usa.png' style={{ height: 32, width: 32 }}/>
+                : <img onClick={() => changeLanguage('en')} alt='Norwegian Flag' src='/images/norway.png' style={{ height: 32, width: 32 }}/>
+            }
         </div>
     );
 };
