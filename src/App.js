d520aa3a87cd08f81af75a9c8f11498679fb9a85
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-23 14:27:58 +0200

    Add button group for switching between weather search and new feature interactive map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import WeatherApp from './Components/WeatherApp';
 import UserHeader from './Components/UserHeader';
+import { ButtonGroup, Button } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
-const App = () => {
+const App = () => {    
+  const { t } = useTranslation();
+
   return (
     <div>
       <UserHeader />
+      <ButtonGroup sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 3
+      }}>
+        <Button>{t('title')}</Button>
+        <Button>{t('titleMap')}</Button>
+      </ButtonGroup>
       <WeatherApp />
     </div>
   );
