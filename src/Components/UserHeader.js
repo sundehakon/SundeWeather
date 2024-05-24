3eb54a9133d49cffc5347b880e9969df6e459d5d
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-24 13:40:09 +0200

    Implement typewriting effect greeting to user header

--- src/Components/UserHeader.js
+++ src/Components/UserHeader.js
@@ -3,13 +3,23 @@ import LogoutButton from '../logout';
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useTranslation } from 'react-i18next';
 import { useTheme } from './ThemeContext';
+import Typewriter from 'typewriter-effect';
 
 const UserHeader = () => {
-    const { t } = useTranslation();
     const { isAuthenticated, user } = useAuth0();
     const { isLightMode } = useTheme();
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    const greetings = ['Hello', 'Welcome', 'Hi', 'Howdy', 'Greetings', 'Salutations', 'Hey', 'Hola', 'Bonjour', 'Ciao', 'Namaste', 'Salaam', 'Shalom', 'Konnichiwa', 'Guten Tag', 'Olá', 'Aloha', 'Hej', 'Hei', 'Hallo', 'Hej', 'Hoi'];
+    const shuffledGreetings = shuffleArray(greetings);
 
     return (
         <div>
@@ -21,7 +31,16 @@ const UserHeader = () => {
                 {isAuthenticated && (
                 <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginTop: 2 }}>
                     <img src={user.picture} alt='User profile' style={{ borderRadius: '50%', height: 64, width: 64 }} />
-                    <Typography sx={{ marginTop: 1 }}>{t('welcome')} {user.nickname}!</Typography>
+                    <Typography sx={{ marginTop: 1, display: 'flex' }}>
+                        <Typewriter 
+                            options={{
+                            strings: shuffledGreetings,
+                            autoStart: true,
+                            loop: true
+                            }}
+                        />
+                        , {user.nickname}!
+                    </Typography>
                 </Box>
                 )}
             </Box>
