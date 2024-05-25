2ef07875d8f5d30737893f4dce23b4695467d505
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-25 14:34:21 +0200

    Add Spanish as language option

--- src/Components/LanguageSwitcher.js
+++ src/Components/LanguageSwitcher.js
@@ -20,8 +20,11 @@ const LanguageSwitcher = () => {
                 <img onClick={() => changeLanguage('no')} alt='United States Flag' src='/images/usa.png' style={{ height: 32, width: 32 }}/>
             ) : language === 'no' ? (
                 <img onClick={() => changeLanguage('fr')} alt='Norwegian Flag' src='/images/norway.png' style={{ height: 32, width: 32 }}/>
-            ) : ( 
-                <img onClick={() => changeLanguage('en')} alt='French Flag' src='/images/france.png' style={{ height: 32, width: 32 }}/>
+            ) : language === 'fr' ? ( 
+                <img onClick={() => changeLanguage('es')} alt='French Flag' src='/images/france.png' style={{ height: 32, width: 32 }}/>
+            ) : (
+                <img onClick={() => changeLanguage('en')} alt='Spanish Flag' src='/images/spain.png' style={{ height: 32, width: 32 }}/>
+            
             )}
         </div>
     );
